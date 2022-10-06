62855992a1d6effb018a84bcdd990bdc8b35d531
niteshbalusu <niteshbalusu@icloud.com>
2022-10-06 12:12:53 -0400

    cheat ms teams

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # cheat-ms-teams
+
+## Yarn global install instructions (Requires NodeJs and Yarn)
+
+### For Linux:
+
+Download Node.js
+
+```
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+```
+
+Install it:
+
+```
+sudo apt-get install -y nodejs
+```
+
+### For Mac or Windows download and install from:
+
+https://nodejs.dev/
+
+### Install
+
+```
+npm i -g cheat-ms-teams
+```
+
+### Run App
+
+```
+cheat
+```
---
+++ index.js
@@ -0,0 +1,68 @@
+import { auto, forever } from 'async';
+
+import robot from 'robotjs';
+
+const moveDelayMs = 1000 * 20;
+
+const moveAround = async () => {
+  try {
+    return await auto({
+      // Initial movement
+      initialMove: cbk => {
+        console.log('\nStarting mouse movement...');
+
+        let y = 0;
+        robot.setMouseDelay(2);
+
+        const twoPI = Math.PI * 2.0;
+        const screenSize = robot.getScreenSize();
+        const height = screenSize.height / 2 - 10;
+        const width = screenSize.width;
+
+        for (let x = 0; x < width; x++) {
+          y = height * Math.sin((twoPI * x) / width) + height;
+          robot.moveMouse(x, y);
+        }
+
+        return cbk();
+      },
+
+      // Move every 20 seconds
+      start: [
+        'initialMove',
+        ({}, cbk) => {
+          console.log(`\n\nMouse will now move every ${moveDelayMs / 1000} seconds...`);
+
+          forever(
+            () => {
+              const moveMouse = () => {
+                robot.setMouseDelay(2);
+
+                let y = 0;
+                const twoPI = Math.PI * 2.0;
+                const screenSize = robot.getScreenSize();
+                const height = screenSize.height / 2 - 10;
+                const width = screenSize.width;
+
+                for (let x = 0; x < width; x++) {
+                  y = height * Math.sin((twoPI * x) / width) + height;
+                  robot.moveMouse(x, y);
+                }
+              };
+
+              setInterval(moveMouse, moveDelayMs);
+            },
+            err => {
+              return cbk(err);
+            }
+          );
+          return cbk();
+        },
+      ],
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+moveAround();
